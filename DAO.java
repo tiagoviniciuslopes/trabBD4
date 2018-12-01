4b5f434a1b4cfd37da6c57b8ed932f052c8550e6
tiagoviniciuslopes <tiago.foz@hotmail.com>
2018-12-01 01:06:52 -0200

    busca funcionando para todas as tabelas no primeiro teste

--- DAO.java
+++ DAO.java
@@ -18,7 +18,7 @@ public class DAO {
 		int i =0;
 		
 		SQLConnector connector = new SQLConnector();
-		String query = "select cliente.*,bairro.nomeBairro,rua.nomeRua,cidade.nomeCidade,cidade.idEstado,estado.nomeEstado,email.idEmail,endereco.idCidade,endereco.idRua,endereco.idBairro,email.enderecoEmail,telefone.idTelefone,telefone.numeroTelefone,hidrometro.idHidrometro,hidrometro.idEnderecoEspecifico,hidrometro.registroHidrometro,enderecoespecifico.numeroEndereco,enderecoespecifico.complemento,enderecoespecifico.idEndereco,enderecoespecifico.cepEndereco from cliente INNER JOIN telefone ON cliente.idCliente=telefone.idCliente INNER JOIN email ON cliente.idCliente=email.idCliente INNER JOIN hidrometro ON hidrometro.idCliente=cliente.idCliente INNER JOIN enderecoespecifico ON enderecoespecifico.idEnderecoEspecifico=hidrometro.idEnderecoEspecifico INNER JOIN endereco ON endereco.idEndereco=enderecoespecifico.idEndereco INNER JOIN bairro ON bairro.idBairro=endereco.idBairro INNER JOIN rua ON rua.idRua=endereco.idRua INNER JOIN cidade ON cidade.idCidade=endereco.idCidade INNER JOIN estado ON estado.idEstado=cidade.idEstado where cliente.documentoCliente = '"+documentoCliente+"' AND enderecoespecifico.cepEndereco = '"+cepEndereco+"' AND enderecoespecifico.numeroEndereco = '"+numeroEndereco+"';";
+		String query = "select cliente.*,fatura.idFatura,fatura.mesFatura,fatura.metrosCubicos,fatura.custoMetroCubico,fatura.esgoto,fatura.custoEsgoto,bairro.nomeBairro,rua.nomeRua,cidade.nomeCidade,cidade.idEstado,estado.nomeEstado,email.idEmail,endereco.idCidade,endereco.idRua,endereco.idBairro,email.enderecoEmail,telefone.idTelefone,telefone.numeroTelefone,hidrometro.idHidrometro,hidrometro.idEnderecoEspecifico,hidrometro.registroHidrometro,enderecoespecifico.numeroEndereco,enderecoespecifico.complemento,enderecoespecifico.idEndereco,enderecoespecifico.cepEndereco from cliente INNER JOIN telefone ON cliente.idCliente=telefone.idCliente INNER JOIN email ON cliente.idCliente=email.idCliente INNER JOIN hidrometro ON hidrometro.idCliente=cliente.idCliente INNER JOIN enderecoespecifico ON enderecoespecifico.idEnderecoEspecifico=hidrometro.idEnderecoEspecifico INNER JOIN endereco ON endereco.idEndereco=enderecoespecifico.idEndereco INNER JOIN bairro ON bairro.idBairro=endereco.idBairro INNER JOIN rua ON rua.idRua=endereco.idRua INNER JOIN cidade ON cidade.idCidade=endereco.idCidade INNER JOIN estado ON estado.idEstado=cidade.idEstado INNER JOIN fatura ON fatura.idHidrometro=hidrometro.idHidrometro where cliente.documentoCliente = '"+documentoCliente+"' AND enderecoespecifico.cepEndereco = '"+cepEndereco+"' AND enderecoespecifico.numeroEndereco = '"+numeroEndereco+"';";
 		ResultSet result = connector.executeQuery(query);
 		
 		while (result.next()) {
@@ -29,7 +29,7 @@ public class DAO {
 			if(i==0) {
 				cliente.setIdCliente(result.getInt("idCliente"));
 				cliente.setDocumentoCliente(result.getString("documentoCliente"));
-				cliente.setPessoaJuridica(result.getBoolean("pessoaJuridia"));
+				cliente.setPessoaJuridica(result.getBoolean("pessoaJuridica"));
 				cliente.setNomeCliente(result.getString("nomeCliente"));
 				enderecoEspecifico.setIdEnderecoEspecifico(result.getInt("idEnderecoEspecifico"));
 				enderecoEspecifico.setCepEndereco(result.getString("cepEndereco"));
@@ -52,7 +52,6 @@ public class DAO {
 			telefone.setNumeroTelefone(result.getString("numeroTelefone"));
 			email.setIdEmail(result.getInt("idEmail"));
 			email.setEnderecoEmail(result.getString("enderecoEmail"));
-			
 			fatura.setIdFatura(result.getInt("idFatura"));
 			fatura.setCustoEsgoto(result.getFloat("custoEsgoto"));
 			fatura.setCustoMetroCubico(result.getFloat("custoMetroCubico"));
@@ -60,19 +59,23 @@ public class DAO {
 			fatura.setMesFatura(result.getString("mesFatura"));			
 			fatura.setMetrosCubicos(result.getInt("metrosCubicos"));
 				
-
+			boolean flag=false;
 			for(Telefone t:telefones){
-				if(t.getIdTelefone()==telefone.getIdTelefone()) telefones.remove(t);
+				if(t.getIdTelefone()==telefone.getIdTelefone()) flag=true;
 			}
-			telefones.add(telefone);
-			for(Email t:emails){
-				if(t.getIdEmail()==email.getIdEmail()) emails.remove(t);
+			if(!flag)telefones.add(telefone);
+			flag=false;
+
+			for(Email e:emails){
+				if(e.getIdEmail()==email.getIdEmail()) flag=true;
 			}
-			emails.add(email);
-			for(Fatura t:faturas){
-				if(t.getIdFatura()==fatura.getIdFatura()) faturas.remove(t);
+			if(!flag)emails.add(email);
+			flag=false;
+
+			for(Fatura f:faturas){
+				if(f.getIdFatura()==fatura.getIdFatura()) flag=true;
 			}
-			faturas.add(fatura);
+			if(!flag)faturas.add(fatura);
 
 		}
 		
--- Trab4.java
+++ Trab4.java
@@ -1,9 +1,60 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Trab4 {
 
 	public static void main(String[] args) throws Exception{
+		DAO dao = new DAO();
+		Cliente c = dao.consultar("09722879901", "85867530", "23");
+
+
+		System.out.println("CLIENTE");
+		System.out.println(c.getIdCliente());
+		System.out.println(c.getNomeCliente());
+		System.out.println(c.getPessoaJuridica());
+
+		System.out.println("EMAILS");
+		for(Email e:c.getEmails()){
+			System.out.println(e.getEnderecoEmail());
+		}
+		System.out.println("TELEFONES");
+		for(Telefone t:c.getTelefones()){
+			System.out.println(t.getNumeroTelefone());
+		}
+
+		System.out.println("HIDROMETRO");
+		Hidrometro h = c.getHidrometro();
+		System.out.println(h.getRegistroHidrometro());
+
+		System.out.println("FATURAS");
+		for(Fatura f:h.getFaturas()){
+			System.out.println(f.getIdFatura());
+			System.out.println(f.getMesFatura());
+		}
+
+		System.out.println("ENDERECO");
+		EnderecoEspecifico ee = h.getEnderecoEspecifico();
+		System.out.println(ee.getCepEndereco());
+		System.out.println(ee.getNumeroEndereco());
+		Endereco e = ee.getEndereco();
+
+		Cidade ci = e.getCidade();
+		System.out.println("CIDADE");
+		System.out.println(ci.getNomeCidade());
+
+		Estado es = ci.getEstado();
+		System.out.println("ESTADO");
+		System.out.println(es.getNomeEstado());
+
+		Rua ru = e.getRua();
+		System.out.println("RUA");
+		System.out.println(ru.getNomeRua());
+
+		Bairro ba = e.getBairro();
+		System.out.println("BAIRRO");
+		System.out.println(ba.getNomeBairro());
+
 		
 	}
 
